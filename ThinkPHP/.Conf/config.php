242425e61377fb5db87132ee3cf87e4598d4edee
JayTan1992 <twy31415926@163.com>
2015-09-07 14:16:41 +0800

    Create ThinkPHP frame

---
+++ ThinkPHP/.Conf/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'APP_DEBUG' => true, 		// 开启调试模式
+	'DB_TYPE'=> 'mysql',        // 数据库类型
+	'DB_HOST'=> 'localhost', 	// 数据库服务器地址
+	'DB_NAME'=>'db_database25_copy',  		// 数据库名称
+	'DB_USER'=>'root', 			// 数据库用户名
+	'DB_PWD'=>'admin', 				// 数据库密码
+	'DB_PORT'=>'3306', 			// 数据库端口
+	'DB_PREFIX'=>'a_', 		// 数据表前缀
+);
+?>
